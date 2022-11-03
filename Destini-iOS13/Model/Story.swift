99020de0e9e5ed179cf2053dbf0ae990b26dca9c
akash.a.singh <113594078+arduino-akash@users.noreply.github.com>
2022-11-03 11:08:49 +0530

    Update

--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -2,9 +2,8 @@
 //  Sotry.swift
 //  Destini-iOS13
 //
-//  Created by Angela Yu on 08/08/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
+//  Created by Singh, Akash | RIEPL on 03/11/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
 
 import Foundation
 
